6c145077f64cff4b87a29d1a1b37a15df533d217
Esteban Dalel R <estebandalelr@gmail.com>
2017-04-07 21:40:36 -0500

    Create login block

--- SSC/app/Seguridad/CifradoSimetrico.java
+++ SSC/app/Seguridad/CifradoSimetrico.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Seguridad;
 
 
@@ -12,72 +7,71 @@ import javax.crypto.SecretKey;
 
 public class CifradoSimetrico
 {
-	private SecretKey desKey;
-	private static String PADDING;
-	
-	public CifradoSimetrico(String algortimo)
-	{
-		PADDING = algortimo;
-	}
-	
-	public byte [] cifrar(String text, SecretKey desKey)
-	{
-		byte [] cipheredText;
-		try
-		{
-			this.desKey = desKey;
-			Cipher cipher = Cipher.getInstance(PADDING);
-			byte [] clearText = text.getBytes();
-			cipher.init(Cipher.ENCRYPT_MODE, desKey);
-			cipheredText = cipher.doFinal(clearText);
-			
-//			System.out.println("->Texto cifrado: " + new String (cipheredText));
-			
-			return cipheredText;
-		}
-		catch (Exception e)
-		{
-			System.out.println("Excepci?n cifrando: " + e.getMessage());
-			return null;
-		}
-	}
-	
-	public byte [] cifrarByte(byte [] cif, SecretKey desKey)
-	{
-		byte [] cipheredText;
-		try
-		{
-			this.desKey = desKey;
-			Cipher cipher = Cipher.getInstance(PADDING);
-			cipher.init(Cipher.ENCRYPT_MODE, desKey);
-			cipheredText = cipher.doFinal(cif);
-			
-//			System.out.println("->Texto cifrado: " + new String (cipheredText));
-			
-			return cipheredText;
-		}
-		catch (Exception e)
-		{
-			System.out.println("Excepci?n cifrando: " + e.getMessage());
-			return null;
-		}
-	}
-	
-	public String descifrar(byte [] cipheredText)
-	{
-		try
-		{
-			Cipher cipher = Cipher.getInstance(PADDING);
-			cipher.init(Cipher.DECRYPT_MODE, desKey);
-			byte [] clearText = cipher.doFinal(cipheredText);
-			String s3 = new String(clearText);
-			return s3;
-		}
-		catch (Exception e)
-		{
-			System.out.println("Excepci?n descifrando : " + e.getMessage());
-			return null;
-		}
-	}
-}
+    private SecretKey desKey;
+    private static String PADDING;
 
+    public CifradoSimetrico(String algortimo)
+    {
+        PADDING = algortimo;
+    }
+
+    public byte [] cifrar(String text, SecretKey desKey)
+    {
+        byte [] cipheredText;
+        try
+        {
+            this.desKey = desKey;
+            Cipher cipher = Cipher.getInstance(PADDING);
+            byte [] clearText = text.getBytes();
+            cipher.init(Cipher.ENCRYPT_MODE, desKey);
+            cipheredText = cipher.doFinal(clearText);
+
+//			System.out.println("->Texto cifrado: " + new String (cipheredText));
+
+            return cipheredText;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Excepci?n cifrando: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public byte [] cifrarByte(byte [] cif, SecretKey desKey)
+    {
+        byte [] cipheredText;
+        try
+        {
+            this.desKey = desKey;
+            Cipher cipher = Cipher.getInstance(PADDING);
+            cipher.init(Cipher.ENCRYPT_MODE, desKey);
+            cipheredText = cipher.doFinal(cif);
+
+//			System.out.println("->Texto cifrado: " + new String (cipheredText));
+
+            return cipheredText;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Excepci?n cifrando: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public String descifrar(byte [] cipheredText)
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance(PADDING);
+            cipher.init(Cipher.DECRYPT_MODE, desKey);
+            byte [] clearText = cipher.doFinal(cipheredText);
+            String s3 = new String(clearText);
+            return s3;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Excepci?n descifrando : " + e.getMessage());
+            return null;
+        }
+    }
+}
